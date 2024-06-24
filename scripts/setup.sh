ed683e38d8de0840dc469f75c92b780efa367eae
clintbonnett-acg <80351457+clintbonnett-acg@users.noreply.github.com>
2024-06-24 09:43:18 -0400

    Updated azure core and text analytics versions

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,9 +6,10 @@ sudo apt-get clean
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get update -y -o Acquire::Retries=3 | tee /tmp/update-output.txt && sudo apt-get install python3-pip -y
 
+pip3 install azure-core==1.29.6
 pip3 install azure-identity==1.10.0
 pip3 install azure-keyvault-secrets==4.4.0
-pip3 install azure-ai-textanalytics==5.1.0
+pip3 install azure-ai-textanalytics==5.3.0
 
 cd /home/cloud_user/
 git clone "https://github.com/ACloudGuru-Resources/content-secure-azure-ai-services.git"
